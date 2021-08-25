42d040b22812cc467894b1d8d198959a5ef83374
Sakshi2830 <60890494+Sakshi2830@users.noreply.github.com>
2021-08-26 01:08:24 +0530

    adding style yo quiz file

--- quiz.js
+++ quiz.js
@@ -2,7 +2,7 @@ const quizForm = document.querySelector('.quiz-form');
 const submitAnswerBtn = document.querySelector("#submit-answer-btn");
 const outputEl = document.querySelector("#output");
 
-const correctAnswers = ["90°", "right angled", "acute"];
+const correctAnswers = ["90°", "right angled", "obtuse","yes","Isosceles"];
 
 function calculateScore() {
     let score = 0;
