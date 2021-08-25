728def877350a441a0b18dc42d0a5fdc51e81348
Sakshi2830 <60890494+Sakshi2830@users.noreply.github.com>
2021-08-25 15:38:32 +0530

    Hypotenuse commit

---
+++ hypotenuse.js
@@ -0,0 +1,18 @@
+const sides = document.querySelectorAll(".side-input");
+const hypotenuseBtn = document.querySelector("#hypotenuse-btn");
+const outputEl = document.querySelector("#output");
+
+
+function calculateSmOfSquares(a,b){
+    const sumOfSquares = a*a + b*b;
+    return sumOfSquares;
+}
+
+
+function calculateHypotenuse(){
+   const sumOfSquares= calculateSmOfSquares(Number(sides[0].value),Number(sides[1].value))
+   const lengthOfHypotenuse = Math.sqrt(sumOfSquares);
+   outputEl.innerText = " the Length of Hypotenuse is " + lengthOfHypotenuse
+}
+
+hypotenuseBtn.addEventListener("click" , calculateHypotenuse)
