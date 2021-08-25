392b0c3d5f07c4f4a90800cd51eb7e5b0f9210c5
Sakshi2830 <60890494+Sakshi2830@users.noreply.github.com>
2021-08-25 15:37:38 +0530

    quizs of triangle commit

---
+++ quiz.js
@@ -0,0 +1,23 @@
+const quizForm = document.querySelector('.quiz-form');
+const submitAnswerBtn = document.querySelector("#submit-answer-btn");
+const outputEl = document.querySelector("#output");
+
+const correctAnswers = ["90°", "right angled", "acute"];
+
+function calculateScore() {
+    let score = 0;
+    let index = 0;
+    const formResults = new  FormData(quizForm);
+    for (let value of formResults.values()){
+        if (value === correctAnswers[index]){
+            score = score + 1; 
+         }
+            index = index + 1;
+     }
+    console.log(score);
+    outputEl.innerText = "your score is " + score;
+    
+ }
+
+
+submitAnswerBtn.addEventListener('click', calculateScore)
